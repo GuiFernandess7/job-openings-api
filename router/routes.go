be3227e9161bda85a6b6c13f58555c5c2a43cb1c
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-06-10 21:28:23 -0300

    feat: update handler structure

---
+++ router/routes.go
@@ -0,0 +1,17 @@
+package router
+
+import (
+	handler "github.com/GuiFernandess7/job-openings-api/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes(router *gin.Engine){
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/opening", handler.ReadOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.PUT("/opening", handler.DeleteOpeningHandler)
+		v1.GET("/openings", handler.ReadAllOpeningHandler)
+	}
+}
